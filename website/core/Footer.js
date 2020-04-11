a963ff1dee34a5892606fe4e84daade162fbd10e
Raul Melo <melo.raulf@gmail.com>
2020-04-11 10:19:20 +0200

    tweak footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require("react");
 
 class Footer extends React.Component {
@@ -37,14 +30,19 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl("doc1.html", this.props.language)}>
-              Getting Started (or other categories)
+            <a href={this.docUrl("basic/setup.html", this.props.language)}>
+              Basic
             </a>
-            <a href={this.docUrl("doc2.html", this.props.language)}>
-              Guides (or other categories)
+            <a href={this.docUrl("advanced/intro.html", this.props.language)}>
+              Advanced
+            </a>
+            <a
+              href={this.docUrl("doadvanced/introc3.html", this.props.language)}
+            >
+              HOC
             </a>
             <a href={this.docUrl("doc3.html", this.props.language)}>
-              API Reference (or other categories)
+              Migrating
             </a>
           </div>
           <div>
@@ -70,15 +68,13 @@ class Footer extends React.Component {
           </div>
           <div>
             <h5>More</h5>
-            <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            {/* <a href={`${this.props.config.baseUrl}blog`}>Blog</a> */}
+            <a href={this.props.config.repoUrl}>GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
               data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub"
             >
               Star
@@ -93,36 +89,10 @@ class Footer extends React.Component {
                 </a>
               </div>
             )}
-            {this.props.config.facebookAppId && (
-              <div className="social">
-                <div
-                  className="fb-like"
-                  data-href={this.props.config.url}
-                  data-colorscheme="dark"
-                  data-layout="standard"
-                  data-share="true"
-                  data-width="225"
-                  data-show-faces="false"
-                />
-              </div>
-            )}
           </div>
         </section>
 
-        <a
-          href="https://opensource.facebook.com/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource"
-        >
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="170"
-            height="45"
-          />
-        </a>
-        <section className="copyright">{this.props.config.copyright}</section>
+        {/* <section className="copyright">{this.props.config.copyright}</section> */}
       </footer>
     );
   }
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -99,9 +99,8 @@ const siteConfig = {
   // Show documentation's last update time.
   // enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  repoUrl:
+    "https://github.com/typescript-cheatsheets/react-typescript-cheatsheet",
 };
 
 module.exports = siteConfig;
