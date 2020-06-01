e0dafadae0e895b7772ec73899d06957b484fba4
slorber <lorber.sebastien@gmail.com>
2020-06-01 17:58:38 +0200

    remove FB copyrights

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
