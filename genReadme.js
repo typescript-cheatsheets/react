b715f357376a31e7224d927be5bbc7cbcff21039
Arvind Srinivasan <arvind@cheenu.net>
2020-08-12 12:15:48 +0530

    Testing Retrieval

---
+++ genReadme.js
@@ -0,0 +1,13 @@
+const { Octokit } = require("@octokit/rest");
+const REPO_DETAILS = {
+  owner: process.env.GITHUB_REPOSITORY_OWNER,
+  repo: process.env.GITHUB_REPOSITORY_OWNER,
+};
+const octokit = new Octokit({ auth: `token ${process.env.ENV_GITHUB_TOKEN}` });
+octokit.repos.getContent({
+  ...REPO_DETAILS,
+  path: 'docs/basic/setup.md'
+}).then(result => {
+    const content = Buffer.from(result.data.content, 'base64').toString()
+    console.log(content)
+  })
