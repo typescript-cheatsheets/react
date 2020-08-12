6ab80cbba1f104e9428e2b9f392ef3bae168e9e9
Arvind Srinivasan <arvind@cheenu.net>
2020-08-12 14:48:08 +0530

    Update genReadme.js

--- genReadme.js
+++ genReadme.js
@@ -3,13 +3,13 @@ const { Octokit } = require("@octokit/rest");
 const REPO_INFO = process.env.GITHUB_REPOSITORY.split('/')
 const REPO_DETAILS = {
   owner: REPO_INFO[0],
-  repo: REPO_INFO[1],
+  repo: REPO_INFO[1]
 };
 
 (async function main() {
   try {
     const octokit = new Octokit({ auth: `token ${process.env.ENV_GITHUB_TOKEN}` });
-    const setupMd = await octokit.repos.getContents({
+    const setupMd = octokit.repos.getContents({
       ...REPO_DETAILS,
       path: 'docs/basic/setup.md'
     })
