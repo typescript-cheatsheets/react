fdcceb636dc7b914a0b8f8cdd1926902c012d646
Arvind <arvind@cheenu.net>
2020-08-13 08:14:40 +0530

    Added .DS_Store to gitignore

--- genReadme.js
+++ genReadme.js
@@ -1,33 +1,42 @@
 const { Octokit } = require("@octokit/rest");
-const Toc = require('markdown-toc');
-const Fm = require('front-matter');
+const Toc = require("markdown-toc");
+const Fm = require("front-matter");
 
 const octokit = new Octokit({ auth: `token ${process.env.ENV_GITHUB_TOKEN}` });
-const repo_info = process.env.GITHUB_REPOSITORY.split('/')
+const repo_info = process.env.GITHUB_REPOSITORY.split("/");
 const repo_details = {
   owner: repo_info[0],
-  repo: repo_info[1]
+  repo: repo_info[1],
 };
 
 (async function main() {
   const readme = await getReadme();
-  let oldTocFences = getFenceForSection("setup", true).exec(readme.content)
-  oldTocFences = oldTocFences && oldTocFences[0] // could be null
-  const setupMd = await readContentFromPath('docs/basic/setup.md');
+  const name = "setup";
+  const path = "docs/basic/setup.md";
+  const readmePath = "README.md";
+  let oldTocFences = getFenceForSection(name, true).exec(readme.content);
+  oldTocFences = oldTocFences && oldTocFences[0]; // could be null
+  const setupMd = await readContentFromPath(path);
   try {
-    let newTocFences = generateContentForSection("setup", setupMd, true);
+    let indent = path.split("/").length - 1;
+    let newTocFences = generateContentForSection(
+      "setup",
+      setupMd,
+      true,
+      indent - 1
+    );
     if (newTocFences === oldTocFences) {
-      console.log('NO CHANGE detected in the setupMd, skipping commit')
-      return 
+      console.log("NO CHANGE detected in the setupMd, skipping commit");
+      return;
     }
     let newContents = readme.content.replace(oldTocFences, newTocFences);
     await octokit.repos.createOrUpdateFile({
       ...repo_details,
       content: Buffer.from(newContents).toString("base64"),
-      path: "README.md",
+      path: readmePath,
       message: `Updated README with ${"setup"} on ${new Date().toISOString()}`,
       sha: readme.sha,
-      branch: "master"
+      branch: "master",
     });
   } catch (err) {
     console.error(err);
@@ -45,30 +54,48 @@ async function getReadme() {
 }
 
 async function readContentFromPath(relative_path) {
-    const MdDoc = await octokit.repos.getContents({
-      ...repo_details,
-      path: relative_path
-    })
-    const MdContent = Fm(Buffer.from(MdDoc.data.content, 'base64').toString());
-    const TableOfContents = Toc(MdContent.body).content
-    return {
-      frontmatter: MdContent.attributes, 
-      body : MdContent.body, 
-      toc: TableOfContents
-    }
+  const MdDoc = await octokit.repos.getContents({
+    ...repo_details,
+    path: relative_path,
+  });
+  const MdContent = Fm(Buffer.from(MdDoc.data.content, "base64").toString());
+  const TableOfContents = Toc(MdContent.body).content;
+  return {
+    frontmatter: MdContent.attributes,
+    body: MdContent.body,
+    toc: TableOfContents,
+  };
 }
 
-function getFenceForSection(sectionName, isToc=false) {
-  const name = isToc ? sectionName + '-toc' : sectionName;
-  const START_COMMENT = `<!--START_SECTION:${name}-->`;
-  const END_COMMENT = `<!--END_SECTION:${name}-->`;
-  return new RegExp(`${START_COMMENT}[\\s\\S]+${END_COMMENT}`);
+function getFence(sectionName, isToc = false) {
+  const name = isToc ? sectionName + "-toc" : sectionName;
+  const START_COMMENT = "<!--START_SECTION:" + name + "-->";
+  const END_COMMENT = "<!--END_SECTION:" + name + "-->";
+  return { start: START_COMMENT, end: END_COMMENT };
+}
+function getFenceForSection(sectionName, isToc = false) {
+  const fence = getFence(sectionName, isToc);
+  return new RegExp(fence.start + "[\\s\\S]+" + fence.end);
 }
 
-function generateContentForSection(sectionName, sectionContent, isToc=false) {
-  return isToc ? 
-          `<!--START_SECTION:${sectionName}-->${sectionContent.toc}<!--END_SECTION:${sectionName}-->`
-       : `<!--START_SECTION:${sectionName}-->
-           ${sectionContent.body}  
-          <!--END_SECTION:${sectionName}-->`
+function generateContentForSection(
+  sectionName,
+  sectionContent,
+  isToc = false,
+  indent = 0
+) {
+  const fence = getFence(sectionName, isToc);
+  if (isToc) {
+    let fenceContent = fence.start;
+    let lines = sectionContent.toc.split("\n");
+    for (let i = 0, len = lines.length; i < len; i += 1)
+      fenceContent += "\t".repeat(indent) + lines[i] + "\n";
+    fenceContent += fence.end;
+    return fenceContent;
+  } else {
+    let fenceContent = fence.start;
+    fenceContent += "\n" + sectionContent.body + "\n";
+    fenceContent += fence.end;
+    return fenceContent;
+  }
 }
